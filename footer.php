8856b1c034dba93359bccb3b887acd1dd91bcc8a
Yagyik0926 <chinchuyagyik@gmail.com>
2021-11-02 21:16:38 +0530

    yagy

--- footer.php
+++ footer.php
@@ -12,318 +12,94 @@
 *{
     font-family: sherif;
 }
-/*********footer*******************/
-. FootAdhar_area {
+
+footer-bottom{
+    background-color: rgba(211,211,211,1);
+}
+.lit{
     position: relative;
-    z-index: 1;
-    
-	}
-	.foo_top_header_one {
-    background-color: rgba(242,242,242, 0.5);
-    color: #15151e;
-}
-.section_padding_100_70 {
-    padding-top: 50px;
-    padding-bottom: 35px;
-}
-.foo_top_header_one {
-    color: #15151e;
-}.FootAdhar_part {
-    margin-bottom: 30px;
-}
-.foo_top_header_one .FootAdhar_part > h5 {
-    color: #15151e;
-}
-.FootAdhar_part h4, .FootAdhar_part h5 {
-    margin-bottom: 30px;
-}
-.FootAdhar_single_contact_info > p, .FootAdhar_single_contact_info > h5, .FootAdhar_blog_area > a, .foo_top_header_one .FootAdhar_part > p {
-    color: rgba(0,0,0,.5);
-}
-p, ul li, ol li {
-    font-weight: 300;
-}
-ul {
-    margin: 0;
-    padding: 0;
-}
-.FootAdhar_bottom_header_one {
-    background-color: rgba(191,191,191,.5);
-}
-.section_padding_50 {
-    padding: 20px 0;
-}
-.FootAdhar_bottom_header_one p {
-    color: #15151e;
-    margin: 0;
-}
-p, ul li, ol li {
-    font-weight: 300;
-}
-.FootAdhar_bottom_header_one a {
-    color: inherit;
-    font-size: 14px;
-}
-a, h1, h2, h3, h4, h5, h6 {
-    font-weight: 400;
-}
-.m-top-15 {
-    margin-top: 15px;
-}
-ul {
-    margin: 0;
-    padding: 0;
+    display:block;
 }
 
-.FootAdhar_widget > li {
-    display: inline-block;
+.lit-it a{
+    color: #000;
+    list-style-type: circle;
 }
-p, ul li, ol li {
-    font-weight: 300;
-}
-ol li, ul li {
-    list-style: outside none none;
-}
-.FootAdhar_widget a {
-    border: 1px solid #333;
-    border-radius: 6px;
-    color: #888;
-    display: inline-block;
-    font-size: 13px;
-    margin-bottom: 4px;
-    padding: 7px 12px;
-}
-ul {
-    margin: 0;
-    padding: 0;
-}
-.FootAdhar_links a {
-    border-bottom: 1px solid #333;
-    color: rgba(0,0,0,.5);
-    display: block;
-    font-size: 13px;
-    margin-bottom: 5px;
-    padding-bottom: 10px;
-}
-.FootAdhar_links a {
-    color: rgba(0,0,0,.5);
-    font-size: 13px;
-}
-top-15 {
-    margin-top: 15px;
-}
-.foo_top_header_one .FootAdhar_part > h5 {
-    color: #15151e;
-}
-.FootAdhar_part h4, .FootAdhar_part h5 {
-    margin-bottom: 30px;
-}
-.FootAdhar_social_links > li {
-    display: inline-block;
-}
-p, ul li, ol li {
-    font-weight: 300;
-}
-.FootAdhar_social_links a {
-    border: 1px solid #333;
-    border-radius: 6px;
-    color: #888;
-    display: inline-block;
-    font-size: 13px;
-    margin-bottom: 3px;
-    padding: 7px 12px;
-}
-.FootAdhar_blog_area .FootAdhar_date {
-    color: #27ae60;
-    font-size: 13px;
-    margin-bottom: 5px;
-}
-.FootAdhar_blog_area > p {
-    color: rgba(0,0,0,.5);
-    line-height: 1.3;
-    margin-bottom: 0;
-}
-.FootAdhar_works > a {
-    display: inline-block;
-    float: left;
-    position: relative;
-    width: 33.33333333%;
-    z-index: 1;
-}
-.FootAdhar_thumb {
-    left: 0;
-    position: absolute;
-    top: 0;
-    width: 75px;
-}
-.FootAdhar_links a i {
-    padding-right: 10px;
-}
-  /* :: 18.0 Footer Area CSS */
 
-    .footer_area {
-        position: relative;
-        z-index: 1;
-    }
- .footer_bottom p > i,
-    .footer_bottom p > a:hover {
-        color: #27ae60;
-    }	
-
-    .social_links_area {
-        border-bottom: 1px solid rgba(0,0,0, 0.2);
-        padding: 50px 0 30px 0;
-        text-align: center;
-        position: relative;
-        z-index: 1;
-    }
- .social_links_area > a:hover {
-        color: #27ae60;
-    }
-
-    .inline-style .social_links_area > a:hover {
-        background-color: transparent;
-        color: #27ae60;
-        border: 0px solid black;
-    }
- .single_feature:hover .feature_text h4 {
-        color: #27ae60;
-    }
-.FootAdhar_blog_area {
-    border-bottom: 1px solid #333;
-    margin-bottom: 15px;
-    padding: 0 0 15px 90px;
-    position: relative;
-    z-index: 1;
+.social{
+    background-color: white;
+    border-radius:50%;
+    margin: 3px 4px;
+    font-size: 12px;
 }
-.FootAdhar_links a {
-    border-bottom: 1px solid #333;
-    color: rgba(0,0,0,.5);
-    display: block;
-    font-size: 13px;
-    margin-bottom: 5px;
-    padding-bottom: 10px;
+ .wid, ul{
+    width: fit-content;
+}
+.social a{
+    color: white;
+    border-radius: 50%;
+
+
+}
+.social a img{
+    width: 40px;
+    height: 40px;
 }
 </style>
-  </head>
-  <body>
-  <footer class="FootAdhar_area border-top">
-        <!-- Top Footer Area Start -->
-        <div class="foo_top_header_one section_padding_100_70">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-md-6 col-lg-3">
-                        <div class="FootAdhar_part">
-                            <h5>About Us</h5>
-                            <p>Techtitude tribe was founded by a group of students with a vision of providing an easier way to help others transform their amazing ideas into the real world solutions, so that they may not feel that setting up a startup is a rocket-science. Its motive is also to promote make-in-India moment. It is currently working and helping implement 6 great innovative ideas into the real world so that young entrepreneurs from around the nation may get help. The startups which are being implemented under this organization are ‘Adharshila’, ‘Shilpart’, ‘Handycart’, ‘Drugabuse’, ‘Attachaki’, etc. and more and more startups are being added day-by-day to our organization</p>
-                        </div>
-                        <div class="FootAdhar_part m-top-15">
-                            <h5>Social Links</h5>
-                            <ul class="FootAdhar_social_links">
-                                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i> Facebook</a></li>
-                                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a></li>
-                                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Instagram</a></li>
-                                <li><a href="#"><i class="fa fa-email" aria-hidden="true"></i>Email</a></li>
-                                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i> Linkedin</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-6 col-lg-3">
-                        <div class="FootAdhar_part">
-                            <h5>SERVICES</h5>
-                            <ul class=" FootAdhar_widget">
-                                <li><a href="#">APP DEVELOPMENT</a></li>
-                                <li><a href="#">WEB DEVELOPMENT</a></li>
-                                <li><a href="#">UX/UI DESIGN</a></li>
-                                <li><a href="#">DIGITAL MARKETING</a></li>
-                                <li><a href="#">PROJESCT MANAGEMENT</a></li>
-                                <li><a href="#">BULK MESSAGINHG</a></li>
-                                <li><a href="#">COMIC DESIGN</a></li>
-                                <li><a href="#">STRATEGIC CONSULTING</a></li>
-                            </ul>
-                        </div>
+<footer>
+<hr>
+<div class="container">
+<div class="row">
+<div class="col-lg-3 col-sm-6">
+<div class="container txt">
+<h4> About Us</h4>
+<p style="text-align:justify;">Our motive is also to promote make-in-India moment. We are currently working and helping implement 6 great innovative ideas into the real world so that young entrepreneurs from around the nation may get help.</p>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6">
+<h4>Important links</h4>
+<div class="lit-it pl-3">
+  <a class="lit" href="/">Home</a>
+  <a class="lit" href="/company/">About Us</a>
+  <a class="lit" href="/services/">Service</a>
+  <a class="lit" href="/products/">Products</a>
+  <a class="lit" href="/projects/">Projects</a>
+  <a class="lit" href="/blogs/">Blogs</a>
+  <a class="lit" href="/contact/">Contact Us</a>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6">
+<h4>Services</h4>
+<div class="lit-it pl-3">
+  <a class="lit" href="/services/#app">Android App Development</a>
+  <a class="lit" href="/services/#web">Web App Development</a>
+ 
+  <a class="lit" href="/services/#marketing">Digital Marketing</a>
+  <a class="lit" href="/services/#graphic">Graphic Designing</a>
+  
+</div>
 
-                        <div class="FootAdhar_part m-top-15">
-                            <h5>Important Links</h5>
-                            <ul class="FootAdhar_links">
-                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>HOME</a></li>
-                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>SERVICES</a></li>
-                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>BLOGS</a></li>
-                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>CONTACT</a></li>
-                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>COMPANY</a></li>
-                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>PROJECT</a></li>
-                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>PRODUCT</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-6 col-lg-3">
-                        <div class="FootAdhar_part">
-                            <h5>Latest blogs</h5>
-                            <div class="FootAdhar_blog_area">
-                                <div class="FootAdhar_thumb">
-								<img class="img-fluid" src="https://3.bp.blogspot.com/--C1wpaf_S4M/W7V__10nRoI/AAAAAAAAK24/1NSfapuYSIY0f0wzXY9NgoH0FjQLT07YACKgBGAs/s1600/maxresdefault.jpg" alt="">
-
-                                </div>
-                                <a href="#">Your Blog Title Goes Here</a>
-                                <p class="FootAdhar_date">21 Jan 2018</p>
-                                <p>Lorem ipsum dolor sit amet, consectetur</p>
-                            </div>
-                            <div class="FootAdhar_blog_area">
-                                <div class="FootAdhar_thumb">
-								<img class="img-fluid" src="https://3.bp.blogspot.com/--C1wpaf_S4M/W7V__10nRoI/AAAAAAAAK24/1NSfapuYSIY0f0wzXY9NgoH0FjQLT07YACKgBGAs/s1600/maxresdefault.jpg" alt="">
-                                </div>
-                                <a href="#">Myth and Misconception <br>about entrepreneurship</a>
-                                <p class="FootAdhar_date">10 MAY 2021</p>
-                                <p>certain idea and notions about entrepreneurs </p>
-                            </div>
-                            <div class="FootAdhar_blog_area">
-                                <div class="FootAdhar_thumb">
-								<img class="img-fluid" src="https://3.bp.blogspot.com/--C1wpaf_S4M/W7V__10nRoI/AAAAAAAAK24/1NSfapuYSIY0f0wzXY9NgoH0FjQLT07YACKgBGAs/s1600/maxresdefault.jpg" alt="">
-                                </div>
-                                <a href="#">Startup failure due to <br>inadequate knowledge of digital marketing</a>
-                                <p class="FootAdhar_date">01 June 2021</p>
-                                <p>Many people  having great startup idea but still</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-6 col-lg-3">
-                        <div class="FootAdhar_part">
-                            <h5>Quick Contact</h5>
-                            <div class="FootAdhar_single_contact_info">
-                                <h5>Phone:</h5>
-                                <p> +91 7217281579 <br> +225 869374027</p>
-                            </div>
-                            <div class="FootAdhar_single_contact_info">
-                                <h5>Email:</h5>
-                                <p>support@techtitudetribe.co.in</p>
-                            </div>
-                        </div>
-                        <div class="FootAdhar_part">
-                            <h5>Latest Works</h5>
-                            <div class="FootAdhar_works">
-                                <a class="FootAdhar_works_img" href="img/gallery/1.jpg"><img src="img/gallery/1.jpg" alt=""></a>
-                                <a class="FootAdhar_works_img" href="img/gallery/4.jpg"><img src="img/gallery/4.jpg" alt=""></a>
-                                <a class="FootAdhar_works_img" href="img/gallery/5.jpg"><img src="img/gallery/5.jpg" alt=""></a>
-                                <a class="FootAdhar_works_img" href="img/gallery/7.jpg"><img src="img/gallery/7.jpg" alt=""></a>
-                                <a class="FootAdhar_works_img" href="img/gallery/10.jpg"><img src="img/gallery/10.jpg" alt=""></a>
-                                <a class="FootAdhar_works_img" href="img/gallery/11.jpg"><img src="img/gallery/11.jpg" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Footer Bottom Area Start -->
-        <div class=" FootAdhar_bottom_header_one section_padding_50 text-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <p>© copyright | TECHTITUDE TRIBE All rights reserved.<br>Designed By <a href="#">TechtitudeTribe</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+</div>
+<div class="col-lg-3 col-sm-6">
+<h4>Contact Us</h4>
+<div class="">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3485.113389974443!2d78.27095241435855!3d29.13184576805869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390b972107a88479%3A0xe103047cc77b583!2sTechtiutde%20Tribe%20LLP!5e0!3m2!1sen!2sin!4v1626947675317!5m2!1sen!2sin" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
+</div>
+</div>
+</div>
+<hr>
+<div class="container footer-bottom p-auto">
+<ul class="mx-auto">
+<li class="social"><a target="_blank" href="https://www.facebook.com/techtitudetribellp" ><img src="static/facebook.png" class="img-fluid"></span></a></li>
+<li class="social"><a target="_blank" href="https://www.instagram.com/techtitude_tribe/?utm_medium=copy_link" ><img src="static/instagram.png" class="img-fluid"></a></li>
+<li class="social"><a target="_blank" href="https://www.linkedin.com/company/techtitudetribe/" ><img src="static/linkedin.png" class="img-fluid"></a></li>
+<li class="social"><a target="_blank" href="https://twitter.com/TechtitudeT?s=20" ><img src="static/twitter.png" class="img-fluid"></a></li>
+</ul>
+<p class="wid mx-auto p-auto text-center">&copy copyright  TechtitudeTribe 2021, &reg All rights reserved.</p>
+</div>
+</div>
+</footer>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
